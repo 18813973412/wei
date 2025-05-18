38faa8b604dfdf43eac932753567dfb549f23584
lin <546594050@qq.com>
2025-05-18 22:31:58 +0800

    访问限流

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -13,6 +13,13 @@ use Carbon\Carbon;
 class PasswordController extends Controller
 {
 
+    public function __construct()
+    {
+        $this->middleware('throttle:3,10', [
+            'only' => ['sendResetLinkEmail']
+        ]);
+    }
+
     public function reset(Request $request)
     {
         // 1. 验证数据是否合规
