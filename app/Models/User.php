548a1adeff46aa5a1e38dcf3c40be87027415da1
lin <546594050@qq.com>
2025-05-19 10:26:17 +0800

    首页微博列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,12 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }
 
+    public function feed()
+    {
+        return $this->statuses()
+            ->orderBy('created_at', 'desc');
+    }
+
     /**
      * The attributes that are mass assignable.
      *
